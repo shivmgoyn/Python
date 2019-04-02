f40aeae3fc3e8561dc12442259aacd7b86f97525
shivmgoyn <46083734+shivmgoyn@users.noreply.github.com>
2019-04-02 21:40:21 +0530

    Add files via upload

---
+++ marksheet.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 07:32:21 2019
+
+@author: neet
+"""
+
+print("|" + "-"*71 + "|")
+print("|" + " "*71 + "|")
+print("|" + '\033[1m' + "{:^71}".format("Rajiv Gandhi Proudyogiki Vishwavidyalaya, Bhopal") + "|")
+print("|" + " "*71 + "|")
+print("|" + "-"*71 + "|")
+print("|" + "{:^71}".format("Statement of marks: ") + "|")
+print("|" + "-"*71 + "|")
+print("|" + "{:^13}".format("Name") + "|" + "{:^21}".format(" ") + "|" + "{:^13}".format("Roll No.") + "|" + "{:^21}".format(" ") + "|")
+print("|" + "{:^13}".format("Course") + "|" + "{:^21}".format(" ") + "|" + "{:^13}".format("Branch") + "|" + "{:^21}".format(" ") + "|")
+print("|" + "{:^13}".format("Semester") + "|" + "{:^21}".format(" ") + "|" + "{:^13}".format("Status") + "|" + "{:^21}".format(" ") + "|")
+print("|" + "-"*71 + "|")
+print("|" + "{:^17}".format("Subject") + "|" + "{:^17}".format("Total Credit") + "|" + "{:^17}".format("Earned Credit") + "|" + "{:^17}".format("Grade") + "|")
+print("|" + "-"*71 + "|")
+print("|" + "{:^17}".format("CS302-[T]") + "|" + "{:^17}".format("4") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("CS303-[T]") + "|" + "{:^17}".format("3") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("CS304-[T]") + "|" + "{:^17}".format("3") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("CS305-[T]") + "|" + "{:^17}".format("3") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("ES301-[T]") + "|" + "{:^17}".format("4") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("BT107-[P]") + "|" + "{:^17}".format("2") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("CS303-[P]") + "|" + "{:^17}".format("1") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("CS304-[P]") + "|" + "{:^17}".format("1") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("CS305-[P]") + "|" + "{:^17}".format("1") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "{:^17}".format("CS306-[P]") + "|" + "{:^17}".format("2") + "|" + "{:^17}".format(" ") + "|" + "{:^17}".format(" ") + "|")
+print("|" + "-"*71 + "|")
+print("|" + "{:^22}".format("Result Des.") + "|" + "{:^24}".format("SGPA") + "|" + "{:^23}".format("CGPA") + "|")
+print("|" + "-"*71 + "|")
+print("|" + "{:^22}".format(" ") + "|" + "{:^24}".format(" ") + "|" + "{:^23}".format(" ") + "|")
+print("|" + "-"*71 + "|")
+print("|" + "{:^22}".format("Revaluation Date") + "|" + "{:^24}".format("Revaluation date(late)") + "|" + "{:^23}".format(" ") + "|")
+print("|" + "-"*71 + "|")
+print("|" + "{:^22}".format(" ") + "|" + "{:^24}".format(" ") + "|" + "{:^23}".format(" ") + "|")
+print("|" + "-"*71 + "|")
